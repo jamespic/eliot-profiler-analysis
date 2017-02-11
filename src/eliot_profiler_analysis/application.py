03038d3c6d4e623638b65bbbc5f122bb6b3eaff2
James Pickering <james_pic@hotmail.com>
2017-02-11 02:36:44 +0000

    Allow user to specify database via command line

--- src/eliot_profiler_analysis/application.py
+++ src/eliot_profiler_analysis/application.py
@@ -28,13 +28,23 @@ def app(db):
     )
 
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'database', nargs='?',
+        help='The directory in which to create or open the database. If not'
+        ' supplied, a temporary database will be used'
+    )
+    args = parser.parse_args()
+
     eliot.add_destination(eliot.FileDestination(sys.stderr))
-    tmpdir = tempfile.mkdtemp()
+    dbdir = args.database or tempfile.mkdtemp()
     port = 8034
     try:
-        with Database(tmpdir) as db:
+        with Database(dbdir) as db:
             httpd = Server(('0.0.0.0', port), app(db))
-            print('Serving "{dbdir}" on {port}'.format(dbdir=tmpdir, port=port))
+            print('Serving "{dbdir}" on {port}'.format(dbdir=dbdir, port=port))
             httpd.safe_start()
     finally:
-        shutil.rmtree(tmpdir)
+        if not args.database:
+            shutil.rmtree(dbdir)
